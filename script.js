38d6764e57619046f0a15a7964828677da8395d9
Christina Yiebiyo <christina_reem@yahoo.com>
2022-01-22 11:25:04 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,86 @@
+const API_URL = 'https://api.github.com/users/'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const main = document.getElementById('main')
+
+async function getUser (username) { 
+  try {
+    const { data } =  await axios(API_URL + username)
+    createUserCard(data);
+    getRepos(username)
+      
+  } catch (error) {
+
+      if(error.response.status == 404) {
+          createErrorCard('No profile with this username');
+      }
+  
+  }
+
+}
+
+async function getRepos (username) { 
+    try {
+        const { data } =  await axios(API_URL + username + '/repos?sort=created')
+        addRepos(data);
+          
+      } catch (error) {
+          createErrorCard('Problem fetching repos for this username');
+          
+      }
+}
+
+form.addEventListener('submit', (e) => { 
+    e.preventDefault();
+
+    const user = search.value
+    if(user) { 
+        getUser(user)
+        search.value = ''
+    }
+})
+
+function createUserCard(user) { 
+    const cardHTML = `
+        <div class="card">
+            <div>
+                <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+            </div>
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+                <ul>
+                <li class="info-list">${user.followers}<strong>Followers</strong></li>
+                <li class="info-list">${user.following}<strong>Following</strong></li>
+                <li class="info-list">${user.public_repos} <strong>Repos</strong></li>
+                </ul>
+
+                <div id="repos"></div>
+            </div>
+        </div>
+    `
+    main.innerHTML = cardHTML
+}
+
+function createErrorCard (msg) { 
+    const cardHTML = `
+        <div class = "card">
+            <h1>${msg}</h1>
+        </div>
+    `
+    main.innerHTML = cardHTML
+}
+
+function addRepos(repos) { 
+    const reposEl = document.getElementById('repos')
+    repos.slice(0, 10).forEach((repo) => {
+        const repoEl= document.createElement('a')
+        repoEl.classList.add('repo')
+        repoEl.href = repo.html_url
+        repoEl.target = '_blank'
+        repoEl.innerHTML = repo.name
+
+        reposEl.appendChild(repoEl)
+    })
+} 
